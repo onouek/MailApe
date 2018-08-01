458c31c1f7623079ce0ca3a457efe2e096b8f561
onouek <kohsuke.onoue@gmail.com>
2018-08-01 20:09:32 +0900

    Create register view

--- django/user/views.py
+++ django/user/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+
+class RegisterView(CreateView):
+    template_name = 'user/register.html'
+    form_class = UserCreationForm
+
+    def get_success_url(self):
+        return reverse(settings.LOGIN_REDIRECT_URL)
