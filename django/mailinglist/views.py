fb6158ca38d7f4e8107b2ac0d9612577894c91c4
onouek <kohsuke.onoue@gmail.com>
2018-08-01 19:49:05 +0900

    Add subscriber to mailinglist view

--- django/mailinglist/views.py
+++ django/mailinglist/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DeleteView, DetailView, ListView
 from mailinglist.models import MailingList
-from mailinglist.forms import MailingListForm
+from mailinglist.forms import MailingListForm, SubscriberForm
 from mailinglist.mixins import UserCanUseMailingList
 
 
@@ -31,3 +32,26 @@ class DeleteMailingListView(LoginRequiredMixin, UserCanUseMailingList,
 class MailingListDetailView(LoginRequiredMixin, UserCanUseMailingList,
                             DetailView):
     model = MailingList
+
+
+class SubscribeToMailingListView(CreateView):
+    form_class = SubscriberForm
+    template_name = 'mailinglist/subscriber_form.html'
+
+    def get_initial(self):
+        return {
+            'mailing_list': self.kwargs['mailinglist_pk']
+        }
+
+    def get_success_url(self):
+        return reverse('mailinglist:subscriber_thankyou', kwargs={
+            'pk': self.object.mailing_list.id,
+        })
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        mailing_list_pk = self.kwargs['mailinglist_pk']
+        ctx['mailing_list'] = get_object_or_404(
+            MailingList,
+            pk=mailing_list_pk)
+        return ctx
