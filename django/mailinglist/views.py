4805eb54eb071225472f783415c9a323f1365c03
onouek <kohsuke.onoue@gmail.com>
2018-08-01 19:26:08 +0900

    create mailinglist list view

--- django/mailinglist/views.py
+++ django/mailinglist/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from mailinglist.models import MailingList
 
-# Create your views here.
+
+class MailingListListView(LoginRequiredMixin, ListView):
+
+    def get_queryset(self):
+        return MailingList.objects.filter(owner=self.request.user)
