fb2292f47e78d1d1747ff33bbfb9e919aba10f2a
onouek <kohsuke.onoue@gmail.com>
2018-08-01 19:31:42 +0900

    Add CreateMailingList view

--- django/mailinglist/views.py
+++ django/mailinglist/views.py
@@ -1,9 +1,19 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import CreateView, ListView
 from mailinglist.models import MailingList
-
+from mailinglist.forms import MailingListForm
 
 class MailingListListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return MailingList.objects.filter(owner=self.request.user)
+
+
+class CreateMailingListView(LoginRequiredMixin, CreateView):
+    form_class = MailingListForm
+    template_name = 'mailinglist/mailinglist_form.html'
+
+    def get_initial(self):
+        return {
+            'owner': self.request.user.id,
+        }
