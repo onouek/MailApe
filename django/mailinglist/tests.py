8d629f7593add1e340416655ff4caf55fa361e45
onouek <kohsuke.onoue@gmail.com>
2018-08-03 21:10:23 +0900

    Create TestCase mixin and subscriber creation test

--- django/mailinglist/tests.py
+++ django/mailinglist/tests.py
@@ -1,3 +1,38 @@
+from unittest.mock import patch
+
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from mailinglist.models import Subscriber, MailingList
+
+
+class MockSendEmailToSubscriberTask:
+
+    def setUp(self):
+        self.send_confirmation_email_patch = patch(
+            'mailinglist.tasks.send_confirmation_email_to_subscriber')
+        self.send_confirmation_email_mock = self.send_confirmation_email_patch.start()
+        super().setUp()
+
+    def tearDown(self):
+        self.send_confirmation_email_patch.stop()
+        self.send_confirmation_email_mock = None
+        super().tearDown()
+
+
+class SubscriberCreationTestCase(
+    MockSendEmailToSubscriberTask,
+    TestCase):
+
+    def test_calling_create_queues_confirmation_email_task(self):
+        user = get_user_model().objects.create_user(
+            username='unit test runner'
+        )
+        mailing_list = MailingList.objects.create(
+            name='unit test',
+            owner=user,
+        )
+        Subscriber.objects.create(
+            email='unittest@example.com',
+            mailing_list=mailing_list)
+        self.assertEqual(self.send_confirmation_email_mock.delay.call_count, 1)
