10446ea5891c5e9ee33ab339e4df9c642391f5cf
onouek <kohsuke.onoue@gmail.com>
2018-08-17 19:54:15 +0900

    Add subscriber list & create view APi

--- django/mailinglist/views.py
+++ django/mailinglist/views.py
@@ -165,3 +165,35 @@ class MailingListRetrieveUpdateDestroyView(
     permission_classes = (IsAuthenticated, CanUseMailingList)
     serializer_class = MailingListSerializer
     queryset = MailingList.objects.all()
+
+
+class SubscriberListCreateView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated, CanUseMailingList)
+    serializer_class = SubscriberSerializer
+
+    def get_queryset(self):
+        mailing_list_id = self.kwargs['mailing_list_pk']
+        mailing_list = get_object_or_404(MailingList, id=mailing_list_id)
+        return mailing_list.subscriber_set.all()
+
+    def get_serializer_class(self):
+        return super().get_serializer_class()
+
+    def get_serializer(self, *args, **kwargs):
+        if kwargs.get('data'):
+            data = kwargs.get('data')
+            mailing_list = {
+                'mailing_list': reverse(
+                    'mailinglist:api-mailing-list-detail',
+                    kwargs={'pk': self.kwargs['mailing_list_pk']})
+            }
+            data.update(mailing_list)
+        return super().get_serializer(*args, **kwargs)
+
+
+class SubscriberRetrieveUpdateDestroyView(
+        generics.RetrieveUpdateDestroyAPIView):
+
+    permission_classes = (IsAuthenticated, CanUseMailingList)
+    serializer_class = ReadOnlyEmailSubscriberSerializer
+    queryset = Subscriber.objects.all()
