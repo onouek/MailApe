49c2ec7acdd371416606a03986cccd60b0f93606
onouek <kohsuke.onoue@gmail.com>
2018-08-26 23:16:26 +0900

    Rename settings to common_settings and Change debug and database settings

--- django/config/common_settings.py
+++ django/config/common_settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'omjs8oeh*@_08m4rj(yv(#!__=kz9yj33y*6n-r2lku5#bl2dd'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
 ALLOWED_HOSTS = []
 
@@ -86,11 +86,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mailape',
-        'USER': 'mailape',
-        'PASSWORD': 'development',
-        'HOST': 'localhost',
-        'PORT': '5432',
     }
 }
 
