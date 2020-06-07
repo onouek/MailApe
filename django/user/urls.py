c0d21f6b67efa85c73720e60c1dcda5ad4a6bbff
onouek <kohsuke.onoue@gmail.com>
2020-06-07 22:38:16 +0900

    Update django version to 3

--- django/user/urls.py
+++ django/user/urls.py
@@ -6,7 +6,7 @@ import user.views
 app_name = "user"
 
 urlpatterns = [
-    path("login", LoginView.as_view(), name="login"),
-    path("logout", LogoutView.as_view(), name="logout"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
     path("register", user.views.RegisterView.as_view(), name="register"),
 ]
